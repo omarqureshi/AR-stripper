afff881d2c522d762305b707bbbfd987a4679bdc
Omar Qureshi <omar@omarqureshi.net>
2011-11-01 14:33:56 +0000

    Move to stripper.rb

--- lib/stripper.rb
+++ lib/stripper.rb
@@ -1,5 +1,19 @@
 require "stripper/version"
 
-module Stripper
-  # Your code goes here...
+module ActiveRecord
+  module Stripper
+    def self.included(base)
+      base.class_eval do 
+        def strip_spaces_from_name
+          self.attributes.each do |key, val|
+            self[key] = val.strip if val.respond_to?(:strip)
+            self[key] = nil if val.blank?
+          end
+        end
+        send :before_validation, :strip_spaces_from_name
+      end
+    end
+  end
 end
+
+ActiveRecord::Base.class_eval {include ActiveRecord::Stripper}
